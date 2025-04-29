01cd789422eb14f76a8bb5502f2efa326f47bf1f
Romanenko Illya <romanenkoisnot@gmail.com>
2025-04-29 13:48:09 +0300

    ionic mobile test

---
+++ android/app/src/main/java/vibes/tracker/MainActivity.java
@@ -0,0 +1,5 @@
+package vibes.tracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vibes.tracker',
+  appName: 'vibes tracker',
+  webDir: 'dist/vibestracker-webapp/browser',
+};
+
+export default config;
