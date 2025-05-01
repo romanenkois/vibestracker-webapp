31f7b61e686541a161840dcbadd740fd3c9aa001
Romanenko Illya <romanenkoisnot@gmail.com>
2025-05-01 23:53:45 +0300

    FIX

---
+++ src/app/core/api/extended.api.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { $appConfig } from '@environments';
+import { SimpleItemsSelection, SimpleTimeFrame } from '@types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExtendedApi {
+  private http: HttpClient = inject(HttpClient);
+  private baseUrl = $appConfig.api.BASE_API_URL;
+
+  public uploadUserExtendedHistory(params: {
+    files: any[];
+    token: string;
+  }): Observable<any> {
+    const endpoint = `${this.baseUrl}/user/upload/extended-history`;
+
+    const headers = {
+      Authorization: `${params.token}`,
+    };
+    const body = {
+      extendedhistrory: '1'
+    };
+
+    return this.http.post(endpoint, body, { headers: headers, });
+  }
+}
---
+++ src/app/interface/pages/extended-history/extended-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extended-history',
+  imports: [],
+  templateUrl: './extended-history.component.html',
+  styleUrl: './extended-history.component.scss'
+})
+export class ExtendedHistoryComponent {
+
+}
