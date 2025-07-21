e905cee8fc0a4c15200bbfa7698d0e7069e92914
Romanenko Illya <romanenkoisnot@gmail.com>
2025-07-22 01:13:26 +0300

    final refactor

--- src/app/shared/widgets/index.ts
+++ src/app/shared/widgets/index.ts
@@ -1,1 +1,6 @@
+export { CardSimpleAlbumComponent } from './cards/card-simple-album/card-simple-album.component';
+export { CardSimpleArtistComponent } from './cards/card-simple-artist/card-simple-artist.component';
+export { CardSimpleGenreComponent } from './cards/card-simple-genre/card-simple-genre.component';
+export { CardSimpleTrackComponent } from './cards/card-simple-track/card-simple-track.component';
+
 export { NavBarComponent } from './nav-bar/nav-bar.component';
