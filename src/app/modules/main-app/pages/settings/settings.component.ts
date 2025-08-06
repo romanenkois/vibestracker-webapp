677cd6243c43633e40599eb42a20bb99b23516e1
Romanenko Illya <romanenkoisnot@gmail.com>
2025-08-06 21:36:34 +0300

    fix

--- src/app/modules/main-app/pages/settings/settings.component.ts
+++ src/app/modules/main-app/pages/settings/settings.component.ts
@@ -1,30 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { AuthorizationCommand, ExtendedHistoryCommand, UserCommand } from '@commands';
 import { TranslatePipe } from '../../../../shared/pipes/translate.pipe';
-import { LanguageSelectorComponent } from '@features';
+import { LanguageSelectorComponent, LoadingSpinner } from '@features';
+import { UploadingStatus } from '@types';
 
 @Component({
   selector: 'app-settings',
-  imports: [TranslatePipe, LanguageSelectorComponent],
+  imports: [TranslatePipe, LanguageSelectorComponent, LoadingSpinner],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
 export default class SettingsComponent {
-  private authorizationCommand: AuthorizationCommand =
-    inject(AuthorizationCommand);
-  private extendedHistoryCommand: ExtendedHistoryCommand = inject(
-    ExtendedHistoryCommand,
-  );
-  private userCommand: UserCommand = inject(
-    UserCommand,
-  );
+  private authorizationCommand: AuthorizationCommand = inject(AuthorizationCommand);
+  private extendedHistoryCommand: ExtendedHistoryCommand = inject(ExtendedHistoryCommand);
+  private userCommand: UserCommand = inject(UserCommand);
 
   logOut() {
     this.authorizationCommand.logOut();
   }
 
+  protected deleteExtendedHistoryStatus: WritableSignal<UploadingStatus> = signal('idle');
   deleteExtendedHistory() {
-    this.extendedHistoryCommand.deleteUserExtendedHistory();
+    this.extendedHistoryCommand.deleteUserExtendedHistory().subscribe((status: UploadingStatus) => {
+      this.deleteExtendedHistoryStatus.set(status);
+    });
   }
 
   clearIgnoredTracks() {
